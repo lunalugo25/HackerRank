16fc9011b22fb5630e51017931b45d04df838544
Jorge Luna <lunalugo25@hotmail.com>
2017-08-02 17:07:21 -0500

    Update Day8-DictionariesAndMaps.swift

--- 30DaysOfCode/Day8-DictionariesAndMaps.swift
+++ 30DaysOfCode/Day8-DictionariesAndMaps.swift
@@ -5,7 +5,6 @@ let n = Int(readLine()!)! // Number of test cases
 var phoneBook = [String: String]()
 for _ in 1 ... n { // Loop from 1 to n
     let line = readLine()!.characters.split(separator: " ").map{ String($0) }
-    // do something with input
     if line.count > 1 {
         let name = line[0]
         let number = line[1]
@@ -14,8 +13,6 @@ for _ in 1 ... n { // Loop from 1 to n
 }
 
 while let line = readLine() { 
-     // Read a single line
-    // do something with input
     if let number = phoneBook[line] {
         print("\(line)=\(number)")
     } else {
